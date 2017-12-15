fbb1b10a063a9513dd1c85a684cf5b3be33cac99
HUA-SC <982737919@qq.com>
2017-12-15 11:08:19 +0800

    增加注册功能，密码使用md5加密存入数据库

--- controller/router.js
+++ controller/router.js
@@ -1,10 +1,32 @@
 /**
  * Created by sc on 2017/12/11.
+ * 该文件用于路由
  */
 const db = require('../module/db.js');
+const encrypt = require('../module/md5.js');
 
 
-function findData(req,res,next) {
+
+//注册信息处理
+function register(req,res,next) {
+    let queryDatabase = req.body.database.trim();       //要查询的数据库
+    let queryJson = req.body.queryJson;     //接收接送格式的请求数据
+    queryJson[0].password = encrypt.encryption(queryJson[0].password.toString());   //密码用MD5加密
+
+    db.insertDocument('Q&A',queryDatabase,queryJson,(err,data) => {
+        if(err){
+            res.send(err);
+            console.log(err);
+        }else{
+            res.send(data);        //将查找数据以json格式返回
+
+        }
+    })
+}
+
+
+//查找数据
+function findData(req,res) {
 
     let queryDatabase = req.body.database.trim();       //要查询的数据库
     let queryJson = req.body.queryJson;     //接收接送格式的请求数据
@@ -21,4 +43,10 @@ function findData(req,res,next) {
 
 }
 
-module.exports = {findData};
+//返回请求错误信息
+function errorHandler(err, req, res, next) {
+    res.status(500);
+    res.render('error', { error: err });
+}
+
+module.exports = {register,findData,errorHandler};
